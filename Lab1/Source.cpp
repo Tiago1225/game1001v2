ed9f549db6b436edb91e673bcb984c4f994addd2
tiago <101339716@georgebrown.ca>
2021-01-21 14:15:25 -0500

    testing coding

---
+++ Lab1/Source.cpp
@@ -0,0 +1,10 @@
+// simple c++ program
+#include <iostream>
+using namespace std;   //use the standard namespace
+
+// main entry point to application
+int main()
+{
+	cout << "programming is great fun!";
+	return 0;
+}
---
+++ lab demos/Source.cpp
@@ -0,0 +1,11 @@
+// simple c++ program
+#include <iostream>
+#include <string>
+using namespace std;   //use the standard namespace
+
+// main entry point to application
+int main()
+{
+	
+		return 0;
+}
