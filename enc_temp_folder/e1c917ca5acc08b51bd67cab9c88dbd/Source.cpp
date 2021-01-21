d00f40189fcd38d64fdf9ae435be26b33f0d0af9
tiago <101339716@georgebrown.ca>
2021-01-21 14:52:26 -0500

    lab 1

---
+++ enc_temp_folder/e1c917ca5acc08b51bd67cab9c88dbd/Source.cpp
@@ -0,0 +1,20 @@
+// simple c++ program
+#include <iostream>
+#include <string>
+using namespace std;   //use the standard namespace
+
+// main entry point to application
+int main()
+{
+	double first = 5.38,
+		second = 58,
+		third = 12.25,
+		forth = 20;
+
+		cout << "addition: " << first + second << endl;
+		cout << "subtraction: " << second - third << endl;
+		cout << "multiplication: " << third * forth << endl;
+		cout << "division: " << forth / first << endl;
+		
+		return 0;
+}
---
+++ lab demos/lab1.cpp
@@ -0,0 +1,36 @@
+// simple c++ program
+#include <iostream>
+#include <string>
+using namespace std;   //use the standard namespace
+
+// main entry point to application
+int main()
+{
+	// data types in order
+	bool
+	char
+	short
+	int
+	long
+	float 
+	long long int
+	string
+
+
+
+
+		// Demontration of all 5 arithmetic operators
+
+	double first = 5.38,
+		second = 58,
+		third = 12.25,
+		forth = 20;
+
+		cout << "addition: " << first + second << endl;
+		cout << "subtraction: " << second - third << endl;
+		cout << "multiplication: " << third * forth << endl;
+		cout << "division: " << forth / first << endl;
+		cout << "modulus" << (int)first % (int)second << endl;
+		
+		return 0;
+}
